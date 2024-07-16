9010b0a6647b31c5d1d262b5de3013e1ec21df16
Hydran00 <davide.nardi-1@studenti.unitn.it>
2024-07-16 13:52:29 +0200

    tmp

--- temp/test.py
+++ temp/test.py
@@ -4,24 +4,22 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import numpy as np
-import smplx
 import torch
-### example API
-# model = smplx.create(model_folder, model_type=model_type,
-#              gender=gender, use_face_contour=use_face_contour,
-#              num_betas=num_betas,
-#              num_expression_coeffs=num_expression_coeffs,
-#              ext=ext)
-# output = model(betas=betas, expression=expression,
-#        return_verts=True)
+import sys
+import numpy as np
 import open3d as o3d
 from joint_names import JOINT_NAMES
+import smplx
 
 NUM_BETAS = 10;
 NUM_EXPRESSIONS = 10;
-NUM_JOINTS = 55;
-
+NUM_JOINTS = 24;
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# VISUALIZE
+
+# show wireframe
 
 class SMPLXVisualizer(Node):
     def __init__(self):
@@ -32,39 +30,50 @@ class SMPLXVisualizer(Node):
             self.listener_callback,
             10)
         self.subscription
-        self.model = smplx.create('../models/SMPLX_MALE.npz', model_type='smplx',
-            gender='male')
+        self.smplx_model = smplx.create('../models/smplx/SMPLX_MALE.npz', model_type='smplx',
+                                  gender='male', ext='npz', num_betas=NUM_BETAS, num_expressions=NUM_EXPRESSIONS,
+                                  use_face_contour=False, num_joints=NUM_JOINTS).to(DEVICE)
+                                  
+        self.betas = torch.tensor(np.zeros(NUM_BETAS)).to(DEVICE) 
+        self.expression = torch.tensor(np.zeros(NUM_EXPRESSIONS)).to(DEVICE)
+        self.pose = torch.tensor(np.zeros(3*NUM_JOINTS)).to(DEVICE)
+        self.viz = o3d.visualization.Visualizer()
+        self.viz.create_window()
+        opt = self.viz.get_render_option()
+        opt.show_coordinate_frame = True
+        opt.background_color = np.asarray([0.5, 0.5, 0.5])
+        opt.mesh_show_wireframe = True
+        self.first = True
+        
+        print("Node Started")
         
     def listener_callback(self, msg):
         parameters = np.array(msg.data)
+
+        self.betas = torch.tensor(parameters[:NUM_BETAS]).reshape(1, NUM_BETAS).to(DEVICE)
+        self.expression = torch.tensor(parameters[NUM_BETAS:NUM_BETAS+NUM_EXPRESSIONS]).reshape(1, NUM_EXPRESSIONS).to(DEVICE)
+        self.pose = torch.tensor(parameters[NUM_BETAS+NUM_EXPRESSIONS: NUM_BETAS+NUM_EXPRESSIONS+3*NUM_JOINTS]).reshape(1, 3*NUM_JOINTS).to(DEVICE)        
         
-        if parameters.size != 185:
-            self.get_logger().error(f"Received unexpected number of parameters: {parameters.size}")
-            return
-
-        self.betas = torch.tensor([parameters[:NUM_BETAS]])  
-        self.expressions = torch.tensor([parameters[NUM_BETAS:NUM_BETAS+NUM_EXPRESSIONS]])
-        self.joint_rotations = []
-        for i in range(21):
-            self.joint_rotations.append([parameters[NUM_BETAS+NUM_EXPRESSIONS+3*i], parameters[NUM_BETAS+NUM_EXPRESSIONS+3*i+1], parameters[NUM_BETAS+NUM_EXPRESSIONS+3*i+2]])
-        self.joint_rotations = torch.tensor(self.joint_rotations)
-            
-        print("betas:",self.betas.shape)
-        print("expressions:",self.expressions.shape)
-        print("joint_rotations:",self.joint_rotations.shape)
-
         self.visualize_model()
 
     def visualize_model(self):
-        
 
-        output = self.model(betas=self.betas, expression=self.expressions, body_pose=self.joint_rotations, return_verts=True) 
-        vertices = output.vertices
-        faces = output.faces
-        mesh = o3d.geometry.TriangleMesh()
-        mesh.vertices = o3d.utility.Vector3dVector(vertices)
-        mesh.triangles = o3d.utility.Vector3iVector(faces)
-        o3d.visualization.draw_geometries([mesh])
+        if self.first:
+            self.mesh = o3d.geometry.TriangleMesh()
+            
+        output = self.smplx_model(betas=self.betas, expression=self.expression)
+        vertices = output.vertices[0].detach().cpu().numpy() 
+        faces = self.smplx_model.faces
+        self.mesh.vertices = o3d.utility.Vector3dVector(vertices)
+        self.mesh.triangles = o3d.utility.Vector3iVector(faces)
+        
+        if self.first:
+            self.viz.add_geometry(self.mesh)
+            self.first = False
+        
+        self.viz.update_geometry(self.mesh)
+        self.viz.poll_events()
+        self.viz.update_renderer()
         
 def main(args=None):
     rclpy.init(args=args)
